2e9e9ffa7aebabbf17ec6d0c4ea0ee856f1fc031
Menno van Leeuwen <menno@vleeuwen.me>
2023-07-13 16:01:20 +0200

    Added filter ability

--- lib/routes/stable_diffusion_router.dart
+++ lib/routes/stable_diffusion_router.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -14,20 +15,63 @@ class StableDiffusionRouter {
     router.post('/<route>', (Request req, String route) async {
       final api = serviceCollection.get<A1111Api>();
       final path = api.getApiUrl('${req.handlerPath}$route');
+      var body = await req.readAsString();
+
+      if (path.path.endsWith('/sdapi/v1/txt2img') && File('./filters.json').existsSync()) {
+        final filters = jsonDecode(File('./filters.json').readAsStringSync());
+        final ttiRequest = jsonDecode(body);
+        String prompt = ttiRequest['prompt'];
+        String negativePrompt = ttiRequest['negative_prompt'];
+
+        for (final filter in filters['banned_words']) {
+          if (prompt.contains(filter)) {
+            print('Filtering banned word $filter from positive prompt');
+          }
+          if (negativePrompt.contains(filter)) {
+            print('Filtering banned word $filter from negative prompt');
+          }
+          prompt = prompt.replaceAll(filter, '');
+          negativePrompt = negativePrompt.replaceAll(filter, '');
+        }
+
+        for (final filter in filters['banned_words_positive']) {
+          if (prompt.contains(filter)) {
+            print('Filtering positive banned word $filter from positive prompt');
+            negativePrompt = '$filter,$negativePrompt';
+          }
+          prompt = prompt.replaceAll(filter, '');
+        }
+
+        for (final filter in filters['banned_words_negative']) {
+          if (negativePrompt.contains(filter)) {
+            print('Filtering negative banned word $filter from negative prompt');
+          }
+          negativePrompt = negativePrompt.replaceAll(filter, '');
+        }
+
+        prompt = filters['positiveAddition'] + prompt;
+        negativePrompt = filters['negativeAddition'] + negativePrompt;
+
+        ttiRequest['prompt'] = prompt;
+        ttiRequest['negative_prompt'] = negativePrompt;
+        body = jsonEncode(ttiRequest);
+      }
 
       print('Passing ${req.requestedUri} ${req.method} to $path');
 
+      final headers = Map<String, String>.from(req.headers)..remove('content-length');
       final client = http.Client();
       final request = http.Request('POST', path)
-        ..headers.addAll(req.headers)
-        ..body = await req.readAsString();
+        ..headers.addAll(headers)
+        ..body = body;
+
       final streamedResponse = await client.send(request);
       final responseBody = jsonDecode(await streamedResponse.stream.bytesToString());
-      print('Returning with response ${streamedResponse.statusCode} body:');
-      try {
-        printLimitedString(jsonEncode(responseBody));
-        // ignore: empty_catches
-      } catch (e) {}
+      // print('Returning with response ${streamedResponse.statusCode} body:');
+      // try {
+      //   printLimitedString(jsonEncode(responseBody));
+      //   // ignore: empty_catches
+      // } catch (e) {}
 
       return Response(streamedResponse.statusCode, body: jsonEncode(responseBody));
     });
